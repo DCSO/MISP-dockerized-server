8039c1fed7e37d66a5a2c8434550e2dc86768eee
8ear <maximilian.haeckl@dcso.de>
2018-04-09 18:54:47 +0200

    Add new build arguments

--- .travis/build.sh
+++ .travis/build.sh
@@ -17,6 +17,11 @@ docker build \
         --build-arg GIT_REPO="$GIT_REPO" \
         --build-arg VCS_REF=$(git rev-parse --short HEAD) \
         --build-arg VERSION="$VERSION" \
+        --build-arg MISP_TAG="$MISP_TAG" \
+        --build-arg python_cybox_TAG="$python_cybox_TAG" \
+        --build-arg python_stix_TAG="$python_stix_TAG" \
+        --build-arg mixbox_TAG="$mixbox_TAG" \
+        --build-arg cake_resque_TAG="$cake_resque_TAG" \
     -f $SCRIPTPATH/../$FOLDER/$DOCKERFILE_PATH -t $DOCKER_REPO:$FOLDER -t $DOCKER_REPO-private:$FOLDER $SCRIPTPATH/../$FOLDER/
 
 # ##################################################
