f86cca849b408c548962493d53832aba1f024f41
8ear <github@8ear.de>
2018-06-12 08:46:04 -0700

    Update 2.4.90

--- 2.4.90-ubuntu/files/entrypoint_apache.sh
+++ 2.4.90-ubuntu/files/entrypoint_apache.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
+function init_apache() {
 # Apache gets grumpy about PID files pre-existing
 rm -f /run/apache2/apache2.pid
 # start Workers for MISP
 su -s /bin/bash -c "/var/www/MISP/app/Console/worker/start.sh" www-data
 
 #exec apache2 -DFOREGROUND
-/usr/sbin/apache2ctl -DFOREGROUND
+/usr/sbin/apache2ctl -DFOREGROUND $1
+}
+
+init_apache $CMD_APACHE
