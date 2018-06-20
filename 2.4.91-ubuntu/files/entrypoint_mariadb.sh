06f8f16e0c2a12a32464f1d699e0559dcdeff6c5
8ear <github@8ear.de>
2018-06-20 21:51:34 +0200

    Update Server Configuration to support integrated redis and mysql

--- 2.4.91-ubuntu/files/entrypoint_mariadb.sh
+++ 2.4.91-ubuntu/files/entrypoint_mariadb.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 set -e
 
-###############################
-echo "started mysql witch CMD: '$CMD_MYSQL'"
-# create socket folder
-mkdir -p /var/run/mysqld
-# change ownership to mysql user and group
-chown -R mysql.mysql /var/run/mysqld
+DATADIR="/var/lib/mysql"
 
-# debian.cnf
+start_mysql(){
+    if [ -z "$@" ]; then
+        gosu mysql mysqld 
+    else
+        gosu mysql $@
+    fi
+}
+
+########################################################
+# create socket folder
+[ ! -d "/var/run/mysqld" ] && echo "mkdir -p /var/run/mysqld" && mkdir -p /var/run/mysqld
+########################################################
+# change ownership to mysql user and group
+echo "chown -R mysql.mysql /var/run/mysqld" && chown -R mysql.mysql /var/run/mysqld
+########################################################
+# create Directory
+echo "########################"
+if [ ! -d "$DATADIR/mysql" ] 
+    then
+        echo "mkdir -p $DATADIR/mysql" && mkdir -p $DATADIR/mysql
+        echo "chown -R mysql.mysql $DATADIR/mysql" && chown -R mysql.mysql $DATADIR/mysql
+        echo 'Initializing database'
+        # "Other options are passed to mysqld." (so we pass all "mysqld" arguments directly here)
+        gosu mysql mysql_install_db --datadir="$DATADIR" --rpm "${@:2}"
+        echo 'Database initialized'
+fi
+
+########################################################
+# create debian.cnf
 debian_conf=/etc/mysql/debian.cnf
+
 # add debian.cnf File
 cat << EOF > $debian_conf
 #	MYSQL Configuration from DCSO
@@ -26,8 +50,97 @@ socket   = /var/run/mysqld/mysqld.sock
 basedir  = /usr
 
 EOF
+########################################################
+echo "########################"
+echo "Start mysqld to setup"
+#"$@" --skip-networking --socket="${SOCKET}" &
+start_mysql &
+pid="$!"
+sleep 2
+# test if mysqld is ready
+i=0
+while(true)
+do
+    [ -z "$(mysql -uroot -h localhost -e 'select 1;'|tail -1|grep ERROR)" ] && break;
+    echo "not ready..."
+    sleep 3
+    i+=1
+    [ "$i" >= 10 ] && echo "can't start DB" && exit 1
+done
+########################################################
+echo "########################"
+# Create Root Password if none is given
+if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+    export MYSQL_ROOT_PASSWORD="$(</dev/urandom tr -dc A-Za-z0-9 | head -c 28)"
+    echo "GENERATED ROOT PASSWORD: $MYSQL_ROOT_PASSWORD"
+fi
 
+
+mysql -uroot -h localhost << EOF
+-- What's done in this file shouldn't be replicated
+--  or products like mysql-fabric won't work
+SET @@SESSION.SQL_LOG_BIN=0;
+
+-- Delete all Users except root
+DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost', '$HOSTNAME') ;
+-- Set Password
+UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root' ;
+--SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+
+-- Create Root User with %
+CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+
+-- Grant Permissions
+GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+
+-- Create MISP DB
+CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;
+
+-- Create MISP DB User    
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;
+GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;
+
+DROP DATABASE IF EXISTS test ;
+FLUSH PRIVILEGES ;
+EOF
+
+# #-- What's done in this file shouldn't be replicated
+# #--  or products like mysql-fabric won't work
+# send_2_mysql "SET @@SESSION.SQL_LOG_BIN=0;"
+
+# #-- Delete all Users except root
+# send_2_mysql "DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost', '$HOSTNAME') ;"
+# #-- Set Password
+# send_2_mysql "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root'"
+# #--SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+
+# #-- Create Root User with Hostname
+# send_2_mysql "CREATE USER 'root'@'${MYSQL_ROOT_HOST}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;"
+
+# #-- Grant Permissions
+# send_2_mysql "GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;"
+# send_2_mysql "GRANT ALL ON *.* TO 'root'@'${MYSQL_ROOT_HOST}' WITH GRANT OPTION ;"
+
+# #-- Create MISP DB
+# send_2_mysql "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;"
+
+# #-- Create MISP DB User    
+# send_2_mysql "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;"
+# send_2_mysql "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;"
+
+# send_2_mysql "DROP DATABASE IF EXISTS test ;"
+# send_2_mysql "FLUSH PRIVILEGES ;"
+
+# import MISP DB Scheme
+echo "########################"
+echo "Import MySQL scheme"
+mysql -u$MYSQL_USER -h$MYSQL_HOST -p$MYSQL_PASSWORD $MYSQL_DATABASE < /var/www/MISP/INSTALL/MYSQL.sql
 #############################
 
-# Start MySQL DB
-/init_mariadb.sh $CMD_MYSQL
+
+echo "########################"
+echo "stopping mysql..."
+service mysql stop
+echo "start longtime mysql..."
+start_mysql
--- 2.4.91-ubuntu/files/entrypoint_redis.sh
+++ 2.4.91-ubuntu/files/entrypoint_redis.sh
@@ -2,16 +2,20 @@
 set -e
 
 
-function init_redis() {
-# allow the container to be started with `--user`
-[ -d "/redis_data_dir" ] || mkdir -p /redis_data_dir
-pushd /redis_data_dir
-if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-	chown -R redis .
-	exec gosu redis "$0" "$@"
-fi
 
-exec "$@"
+function init_redis() {
+	# allow the container to be started with `--user`
+	[ -d "/redis_data_dir" ] || mkdir -p /redis_data_dir
+	pushd /redis_data_dir
+	if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+		chown -R redis .
+		exec gosu redis "$0" "$@"
+	fi
+
+	echo "####################################"
+	echo "started REDIS with cmd: '$CMD_REDIS'"
+	echo "####################################"
+	exec "$@"
 }
 
 init_redis $CMD_REDIS
