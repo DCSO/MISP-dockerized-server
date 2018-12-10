4712d7d16b6fd078918864ee2bff04fa049c4c96
Max H <10329648+8ear@users.noreply.github.com>
2018-12-10 08:04:03 +0100

    Feat.MDD139 - Add MISP Version 2.4.99 (#16)
    ## Changelog for feat.MDD139 - Add MISP Version 2.4.99
    ### Update Informations 
    Features of the release:
    - Added MISP-Server Container for MISP release 2.4.99.
      For more information please have a look at the official changelog at https://github.com/MISP/MISP/releases/tag/v2.4.99
    - Added gitlab-ci test job
    
    ### General Changes
    Besides the new MISP version, no general changes were made.
    
    ### Fixes & Improvements
    - Added MISP-Server Container with MISP Version 2.4.99
    - Added gitlab-ci test job
    
    ### Detailed Changes
    - Added new MISP Server 2.4.99 on base of 2.4.97.
    - Gitlab-CI can now test the server before it is merged to master.

---
+++ 2.4.99-debian/files/entrypoint_cron.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+
+CAKE="/var/www/MISP/app/Console/cake"
+
+
+
+# SLEEP 1h
+sleep 3600
+
+
+pushd /var/www/MISP/app
+
+[ -z $AUTH_KEY ] && export AUTH_KEY=$(mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE -e "SELECT authkey FROM users;" | head -2|tail -1)
+
+# Wait until database is ready then test    
+while(true)
+do
+    [ -z "$(mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -e 'select 1;'|tail -1|grep ERROR)" ] && [ ! -z "$($CAKE Admin getSetting MISP.baseurl|grep value)" ] && break;
+    sleep 3
+done
+
+# Administering MISP via the CLI
+    # Certain administrative tasks are exposed to the API, these help with maintaining and configuring MISP in an automated way / via external tools.:
+    # GetSettings: MISP/app/Console/cake Admin getSetting [setting]
+    # SetSettings: MISP/app/Console/cake Admin getSetting [setting] [value]
+    # GetAuthkey: MISP/app/Console/cake Admin getauthkey [email]
+    # SetBaseurl: MISP/app/Console/cake Baseurl setbaseurl [baseurl]
+    # ChangePassword: MISP/app/Console/cake Password [email] [new_password]
+
+# Automating certain console tasks
+    # If you would like to automate tasks such as caching feeds or pulling from server instances, you can do it using the following command line tools. Simply execute the given commands via the command line / create cron jobs easily out of them.:
+    # Pull: MISP/app/Console/cake Server pull [user_id] [server_id] [full|update]
+    # Push: MISP/app/Console/cake Server push [user_id] [server_id]
+    # CacheFeed: MISP/app/Console/cake Server cacheFeed [user_id] [feed_id|all|csv|text|misp]
+    # FetchFeed: MISP/app/Console/cake Server fetchFeed [user_id] [feed_id|all|csv|text|misp]
+    # Enrichment: MISP/app/Console/cake Event enrichEvent [user_id] [event_id] [json_encoded_module_list]
+
+while(true)
+do
+    # START the SCRIPT
+    COUNTER="`date +%Y-%m-%d_%H:%M`"
+    echo "[ $COUNTER ] Start MISP-dockerized Cronjob at `date +%Y-%m-%d_%H:%M`... "
+
+
+    #If you would like to automate tasks such as caching feeds or pulling from server instances, you can do it using the following command line tools. Simply execute the given commands via the command line / create cron jobs easily out of them.:
+    #Pull: MISP/app/Console/cake Server pull [user_id] [server_id] [full|update]
+
+    echo "$CAKE Server pull 1 update..." && $CAKE Server pull 1 update
+
+    # CacheFeed: MISP/app/Console/cake Server cacheFeed [user_id] [feed_id|all|csv|text|misp]
+    echo "$CAKE Server cacheFeed 1 all..." && $CAKE Server cacheFeed 1 all
+
+    #FetchFeed: MISP/app/Console/cake Server fetchFeed [user_id] [feed_id|all|csv|text|misp]
+    echo "$CAKE Server fetchFeed 1 all..." && $CAKE Server fetchFeed 1 all
+    # Finished
+    echo "[ $COUNTER ] Finished MISP-dockerized Cronjob at `date +%Y-%m-%d_%H:%M`... "
+    sleep 3600
+done
