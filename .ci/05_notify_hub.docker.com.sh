de9fc4064693e0e527d873d009f8f6d012d673ae
8ear <github@8ear.de>
2019-02-04 16:54:04 +0100

    Test new notify tool

--- .ci/05_notify_hub.docker.com.sh
+++ .ci/05_notify_hub.docker.com.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 STARTMSG="[notify_hob.docker.com]"
 
+DOCKER_SLUG="$1"
+TOKEN="$2"
+
 echo "$STARTMSG Notify hub.docker.com"
 
-# Docker Repo e.g. dcso/misp-dockerized-proxy
-[ -z "$(git remote get-url origin|grep git@)" ] || GIT_REPO="$(git remote get-url origin|sed 's,.*:,,'|sed 's,....$,,')"
-[ -z "$(git remote get-url origin|grep http)" ] || GIT_REPO="$(git remote get-url origin|sed 's,.*github.com/,,'|sed 's,....$,,')"
-[ -z "$(echo $GIT_REPO|grep $GITLAB_HOST)" ] ||  GIT_REPO="$(git remote get-url origin|sed 's,.*'${GITLAB_HOST}'/'${GITLAB_GROUP}'/,,'|sed 's,....$,,')"
+# Find Out Git Hub Repository
+echo "$STARTMSG Set GIT_REPO..."
+if [ ! -z "$(git remote get-url origin|grep git@)" ]
+then
+    GIT_REPO="$(git remote get-url origin|sed 's,.*:,,'|sed 's,....$,,')"
+elif [ ! -z "$(git remote get-url origin|grep http)" ] 
+then    
+    GIT_REPO="$(git remote get-url origin|sed 's,http.*//.*/,,'|sed 's,....$,,')"
+elif [ ! -z "$(echo $GIT_REPO|grep $GITLAB_HOST)" ] 
+then
+    GIT_REPO="$(git remote get-url origin|sed 's,.*'${GITLAB_HOST}'/'${GITLAB_GROUP}'/,,'|sed 's,....$,,')"
+else
+    echo "Can not found the Git URL. Exit now."
+    exit 1
+fi
 
-DOCKER_REPO="dcso/$(echo $GIT_REPO|cut -d / -f 2|tr '[:upper:]' '[:lower:]')"
 
-curl -X POST -H "Content-Type: application/json"  --data '{"docker_tag_name": "hub_automatic_untested"}'  https://registry.hub.docker.com/u/$DOCKER_REPO/trigger/$1/
+DOCKER_REPO="$DOCKER_SLUG/$(echo $GIT_REPO|cut -d / -f 2|tr '[:upper:]' '[:lower:]')"
+
+curl -X POST -H "Content-Type: application/json"  --data '{"docker_tag_name": "hub_automatic_untested"}'  https://registry.hub.docker.com/u/$DOCKER_REPO/trigger/$TOKEN/
 
 echo "$STARTMSG $0 is finished."
