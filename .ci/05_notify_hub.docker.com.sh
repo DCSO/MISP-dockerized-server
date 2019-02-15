6e7a818387b74593803933f37c47f4e4c7391631
8ear <github@8ear.de>
2019-02-15 17:11:19 +0100

    Add 2.4.102 in test

--- .ci/05_notify_hub.docker.com.sh
+++ .ci/05_notify_hub.docker.com.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 STARTMSG="[notify_hob.docker.com]"
 
-DOCKER_SLUG="$1"
-TOKEN="$2"
+NOTIFY_URL="$1"
 
 echo "$STARTMSG Notify hub.docker.com"
 
-# Find Out Git Hub Repository
-echo "$STARTMSG Set GIT_REPO..."
-if [ ! -z "$(git remote get-url origin|grep git@)" ]
-then
-    GIT_REPO="$(git remote get-url origin|sed 's,.*:,,'|sed 's,....$,,')"
-elif [ ! -z "$(git remote get-url origin|grep http)" ] 
-then    
-    GIT_REPO="$(git remote get-url origin|sed 's,http.*//.*/,,'|sed 's,....$,,')"
-elif [ ! -z "$(echo $GIT_REPO|grep $GITLAB_HOST)" ] 
-then
-    GIT_REPO="$(git remote get-url origin|sed 's,.*'${GITLAB_HOST}'/'${GITLAB_GROUP}'/,,'|sed 's,....$,,')"
-else
-    echo "Can not found the Git URL. Exit now."
-    exit 1
-fi
-
-
-DOCKER_REPO="$DOCKER_SLUG/$(echo $GIT_REPO|cut -d / -f 2|tr '[:upper:]' '[:lower:]')"
-
-curl -X POST -H "Content-Type: application/json"  --data '{"docker_tag_name": "hub_automatic_untested"}'  https://registry.hub.docker.com/u/$DOCKER_REPO/trigger/$TOKEN/
+curl -X POST -H "Content-Type: application/json"  --data '{"docker_tag_name": "hub_automatic_untested"}'  "$NOTIFY_URL"
 
 echo "$STARTMSG $0 is finished."
