29e1ef6748ed0b0300bff80b221e2e3e808d5f8d
Max H <10329648+8ear@users.noreply.github.com>
2019-03-15 11:37:47 +0100

    Feat/MDD-172: Added MISP Version 2.4.102 (#27)
    ## Feat/MDD-172: Added MISP Version 2.4.102
    ### Update Information
    Features of the release:
    - Added MISP server container for MISP release 2.4.102.
      For more information, please have a look at the official changelog at https://github.com/MISP/MISP/releases/tag/v2.4.102.
    - Added improved CI scripts
    ### General Changes
    Besides the new MISP version, we improved the CI scripts for Gitlab and Travis CI.
    ### Fixes and Improvements
    - Added MISP server container with MISP Version 2.4.102
    - Added improved CI scripts
    ### Detailed Changes
    - Added new MISP server 2.4.102 on base of 2.4.97.

--- .ci/04_push.sh
+++ .ci/04_push.sh
@@ -15,33 +15,34 @@ REGISTRY_PW="$3"
 # Find the right Docker Repo name e.g. dcso/misp-dockerized-proxy
 [ -z "$(git remote get-url origin|grep git@)" ] || GIT_REPO="$(git remote get-url origin|sed 's,.*:,,'|sed 's,....$,,')"
 [ -z "$(git remote get-url origin|grep http)" ] || GIT_REPO="$(git remote get-url origin|sed 's,.*github.com/,,'|sed 's,....$,,')"
-if [ ! -z $GITLAB_HOST ]; then
-    [ -z "$(echo $GIT_REPO | grep $GITLAB_HOST)" ] ||  GIT_REPO="$(git remote get-url origin|sed 's,.*'${GITLAB_HOST}'/'${GITLAB_GROUP}'/,,'|sed 's,....$,,')"
+if [ ! -z "$GITLAB_HOST" ]; then
+    [ -z "$(echo "$GIT_REPO" | grep "$GITLAB_HOST")" ] ||  GIT_REPO="$(git remote get-url origin|sed 's,.*'${GITLAB_HOST}'/'${GITLAB_GROUP}'/,,'|sed 's,....$,,')"
 fi
 
 # Set Container Name in lower case
-CONTAINER_NAME="$(echo $GIT_REPO|cut -d / -f 2|tr '[:upper:]' '[:lower:]')"
+CONTAINER_NAME="$(echo "$GIT_REPO"|cut -d / -f 2|tr '[:upper:]' '[:lower:]')"
 
 # Set the right Docker Repository with the Docker registry URL
 DOCKER_REPO="$REGISTRY_URL/$CONTAINER_NAME"
 
 # Find all builded versions of the current Docker image
-ALL_BUILD_DOCKER_VERSIONS=$(docker images --format '{{.Repository}}={{.Tag}}'|grep $DOCKER_REPO|cut -d = -f 2)
+ALL_BUILD_DOCKER_VERSIONS=$(docker images --format '{{.Repository}}={{.Tag}}'|grep "$DOCKER_REPO"|cut -d = -f 2)
 
 # Login to Docker registry
 [ "$REGISTRY_URL" != "dcso" ] && DOCKER_LOGIN_OUTPUT="$(echo "$REGISTRY_PW" | docker login -u "$REGISTRY_USER" "$REGISTRY_URL" --password-stdin)"
-[ "$REGISTRY_URL" == "dcso" ] && DOCKER_LOGIN_OUTPUT="$(echo "$REGISTRY_PW" | docker login -u "$REGISTRY_USER" --password-stdin)"
-echo $DOCKER_LOGIN_OUTPUT
-DOCKER_LOGIN_STATE="$(echo $DOCKER_LOGIN_OUTPUT | grep 'Login Succeeded')"
+[ "$REGISTRY_URL" = "dcso" ] && DOCKER_LOGIN_OUTPUT="$(echo "$REGISTRY_PW" | docker login -u "$REGISTRY_USER" --password-stdin)"
+echo "$DOCKER_LOGIN_OUTPUT"
+DOCKER_LOGIN_STATE="$(echo "$DOCKER_LOGIN_OUTPUT" | grep 'Login Succeeded')"
 
 if [ ! -z "$DOCKER_LOGIN_STATE" ]; then
     # Push all Docker images
     for i in $ALL_BUILD_DOCKER_VERSIONS
     do
-        echo "$STARTMSG docker push $DOCKER_REPO:$i" && docker push $DOCKER_REPO:$i
+        echo "$STARTMSG docker push $DOCKER_REPO:$i" && docker push "$DOCKER_REPO:$i"
+        [ "$?" -ne 0 ] && exit 1
     done
 else
-    echo $DOCKER_LOGIN_OUTPUT
+    echo "$DOCKER_LOGIN_OUTPUT"
     exit
 fi
 
---
+++ 2.4.102-debian/files/entrypoint_local_mariadb.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+set -e
+
+STARTMSG="[ENTRYPOINT_LOCAL_MARIADB]"
+DATADIR="/var/lib/mysql"
+FOLDER_with_VERSIONS="/var/lib/mysql"
+
+[ -z $MYSQL_DATABASE ] && export MYSQL_DATABASE=misp
+[ -z $MYSQL_HOST ] && export MYSQL_HOST=localhost
+[ -z "$MYSQL_ROOT_PASSWORD" ] && echo "$STARTMSG No MYSQL_ROOT_PASSWORD is set. Exit now." && exit 1
+
+
+function upgrade(){
+    for i in $FOLDER_with_VERSIONS
+    do
+        if [ ! -f $i/${NAME} ] 
+        then
+            # File not exist and now it will be created
+            echo ${VERSION} > $i/${NAME}
+        elif [ ! -f $i/${NAME} -a -z "$(cat $i/${NAME})" ]
+        then
+            # File exists, but is empty
+            echo ${VERSION} > $i/${NAME}
+        elif [ "$VERSION" == "$(cat $i/${NAME})" ]
+        then
+            # File exists and the volume is the current version
+            echo "$STARTMSG Folder $i is on the newest version."
+        else
+            # upgrade
+            echo "$STARTMSG Folder $i should be updated."
+
+            ############ DO ANY!!!
+        fi
+    done
+}
+
+function start_mysql(){
+    if [ -z "$@" ]; then
+        gosu mysql mysqld 
+    else
+        gosu mysql $@
+    fi
+}
+
+function init_mysql(){
+
+echo "$STARTMSG Initializing database"
+echo "$STARTMSG mkdir -p $DATADIR/mysql" && mkdir -p $DATADIR/mysql
+echo "$STARTMSG chown -R mysql.mysql $DATADIR/*" && chown -R mysql.mysql $DATADIR/*
+# "Other options are passed to mysqld." (so we pass all "mysqld" arguments directly here)
+gosu mysql mysql_install_db --datadir="$DATADIR" --rpm "${@:2}"
+echo "$STARTMSG Database initialized"
+
+echo "$STARTMSG Start mysqld to setup"
+#"$@" --skip-networking --socket="${SOCKET}" &
+start_mysql &
+pid="$!"
+sleep 2
+# test if mysqld is ready
+i=0
+while(true)
+do
+    [ -z "$(mysql -uroot -h $MYSQL_HOST -e 'select 1;'|tail -1|grep ERROR)" ] && break;
+    echo "$STARTMSG not ready..."
+    sleep 3
+    i+=1
+    [ "$i" >= 10 ] && echo "can't start DB" && exit 1
+done
+########################################################
+
+mysql -uroot -h localhost << EOF
+-- What's done in this file shouldn't be replicated
+--  or products like mysql-fabric won't work
+SET @@SESSION.SQL_LOG_BIN=0;
+
+-- Delete all Users except root
+DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost', '$HOSTNAME') ;
+-- Set Password
+UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root' ;
+--SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+
+-- Create Root User with %
+CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+
+-- Grant Permissions
+GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+
+-- Create MISP DB
+CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;
+
+-- Create MISP DB User    
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;
+GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;
+
+DROP DATABASE IF EXISTS test ;
+FLUSH PRIVILEGES ;
+EOF
+
+########################################################
+# import MISP DB Scheme
+echo "$STARTMSG Import MySQL scheme"
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /var/www/MISP/INSTALL/MYSQL.sql
+
+########################################################
+# create debian.cnf
+debian_conf=/etc/mysql/debian.cnf
+
+# add debian.cnf File
+cat << EOF > $debian_conf
+#	MYSQL Configuration from DCSO
+[client]
+host     = localhost
+user     = root
+password = $(echo $MYSQL_ROOT_PASSWORD)
+socket   = /var/run/mysqld/mysqld.sock
+[mysql_upgrade]
+#host     = localhost
+user     = root
+password = $(echo $MYSQL_ROOT_PASSWORD)
+socket   = /var/run/mysqld/mysqld.sock
+basedir  = /usr
+
+EOF
+    ########################################################
+
+}
+
+
+########################################################
+########################################################
+########################################################
+# START MAIN
+########################################################
+########################################################
+########################################################
+
+
+# create socket folder if not exists
+[ ! -d "/var/run/mysqld" ] && mkdir -p /var/run/mysqld && chown -R mysql.mysql /var/run/mysqld
+########################################################
+# Initialize mysql daemon
+[ ! -d "$DATADIR/mysql" ] && init_mysql
+########################################################
+# check volumes and upgrade if it is required
+echo "$STARTMSG upgrade if it is required..." && upgrade
+########################################################
+# Stop existing mysql deamon
+echo "$STARTMSG stopping mysql..." && service mysql stop
+########################################################
+# Own the directory
+echo "$STARTMSG chown -R mysql.mysql $DATADIR/*" && chown -R mysql.mysql $DATADIR/*
+########################################################
+# CHMOD the configuration files
+echo "$STARTMSG chmod -R 644 /etc/mysql/*" && chmod -R 644 /etc/mysql/*
+########################################################
+# start mysql deamon
+echo "$STARTMSG start longtime mysql..." && start_mysql
