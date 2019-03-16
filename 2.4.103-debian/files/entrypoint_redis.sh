19feb86224a36462b14ea00b95137a8e88a8e433
8ear <github@8ear.de>
2019-03-16 17:21:32 +0100

    fixing redis entrypoint

--- 2.4.103-debian/files/entrypoint_redis.sh
+++ 2.4.103-debian/files/entrypoint_redis.sh
@@ -4,17 +4,21 @@ set -e
 STARTMSG="[ENTRYPOINT_REDIS]"
 REDIS_DATA="/redis_data_dir"
 
-# allow the container to be started with `--user`
-[ -d "$REDIS_DATA" ] || mkdir -p "$REDIS_DATA"
-# change directory
-cd "$REDIS_DATA"
-# check if script is started as user redis if not do it!
-if [ "$(id -u)" = '0' ]; then
-	chown -R redis "$REDIS_DATA"
-	exec gosu redis "$0" "$CMD_REDIS"
+if [ "$REDIS_FQDN" = "localhost" ] || [ -z "$REDIS_FQDN" ] || [ "$REDIS_FQDN" = "misp-server" ]; then
+	# allow the container to be started with `--user`
+	[ -d "$REDIS_DATA" ] || mkdir -p "$REDIS_DATA"
+	# change directory
+	cd "$REDIS_DATA"
+	# check if script is started as user redis if not do it!
+	if [ "$(id -u)" = '0' ]; then
+		chown -R redis "$REDIS_DATA"
+		exec gosu redis "$0" "$CMD_REDIS"
+	fi
+
+	echo "$STARTMSG ###############	started REDIS with cmd: '$CMD_REDIS'	#############"
+
+	redis-server "$CMD_REDIS"
+
+else
+	echo "$STARTMSG Deactivate local Redis server."
 fi
-
-echo "$STARTMSG ###############	started REDIS with cmd: '$CMD_REDIS'	#############"
-
-redis-server "$CMD_REDIS"
-
