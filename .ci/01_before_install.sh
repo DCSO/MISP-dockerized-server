0d786fba1bd5140f189e182f45522aab09c3acef
8ear <github@8ear.de>
2019-04-02 15:03:11 +0200

    Change before_install script

--- .ci/01_before_install.sh
+++ .ci/01_before_install.sh
@@ -5,14 +5,15 @@ STARTMSG="[before_install]"
 # Install Requirements
 echo
 echo "$STARTMSG Install requirements..."
-    [ ! -z "$(which apk)" ] && apk add --no-cache make bash sudo git curl coreutils grep python3 gcc
+    [ ! -z "$(which apk)" ] && apk add --no-cache bash sudo git curl coreutils grep py-pip python-dev libffi-dev openssl-dev gcc libc-dev make
     [ ! -z "$(which apt-get)" ] && apt-get update; 
     [ ! -z "$(which apt-get)" ] && apt-get install make bash sudo git curl coreutils grep python3 gcc
     # Upgrade Docke
     [ ! -z "$(which apt-get)" ] && apt-get install --only-upgrade docker-ce -y
 # Install docker-compose
     # https://stackoverflow.com/questions/42295457/using-docker-compose-in-a-gitlab-ci-pipeline
-    [ -z "$(which docker-compose)" ] && pip3 install --no-cache-dir docker-compose
+    [ -z "$(which docker-compose)" ] && sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+                                        && sudo chmod +x /usr/local/bin/docker-compose
 # Show version of docker-compose:
     docker-compose -v
 
