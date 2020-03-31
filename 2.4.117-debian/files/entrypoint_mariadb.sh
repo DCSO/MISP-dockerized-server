6655009d351ab96b3b9de9ff54320532ab67a5d6
Mezzonian <alexander.heidorn@dcso.de>
2020-03-31 13:27:10 +0200

    rebuild mariadb entrypoint

--- 2.4.117-debian/files/entrypoint_mariadb.sh
+++ 2.4.117-debian/files/entrypoint_mariadb.sh
@@ -49,7 +49,11 @@ check_mysql(){
 
 }
 
-upgrade(){
+upgrade(){# create socket folder if not exists
+    if [ ! -d "/var/run/mysqld" ];then
+        mkdir -p /var/run/mysqld
+        chown -R mysql.mysql /var/run/mysqld
+    fi
     for i in $FOLDER_with_VERSIONS
     do
         if [ ! -f "$i/${NAME}" ] 
@@ -89,8 +93,6 @@ echo "$STARTMSG chown -R mysql.mysql $DATADIR" && chown -R mysql.mysql $DATADIR
 gosu mysql mysql_install_db --datadir="$DATADIR" --rpm "${@:2}"
 echo "$STARTMSG Database initialized"
 
-echo "$STARTMSG Start mysqld to setup"
-start_mysql &
 # test if mysqld is ready
 check_mysql
 echo "$STARTMSG Create database $MYSQL_DATABASE, change root password and add $MYSQL_USER"
--- 2.4.123-ubuntu/files/entrypoint_mariadb2.sh
+++ 2.4.123-ubuntu/files/entrypoint_mariadb2.sh
@@ -58,158 +58,66 @@ start_mysql(){
 }
 
 init_mysql(){
+
     echo "$STARTMSG Initializing database..."
     if [[ ! -e /var/lib/mysql/misp/users.ibd ]]; then
         debug "Setting up database"
+    
+        # Set root password
+        sudo mysql -u ${DBUSER_ADMIN} -p -e "UPDATE mysql.user SET Password=PASSWORD('${DBPASSWORD_ADMIN}') WHERE User='root';"
+        # Remove remote root
+        sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+        # Remove anomynous user 
+        "DELETE FROM mysql.user WHERE User='';"
+        sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "DELETE FROM mysql.user WHERE User='';"
+        # Dropp test db
+        sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "DROP DATABASE IF EXISTS test;"
+        sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
+        # Reload privileges
+        "FLUSH PRIVILEGES;"
+        sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "FLUSH PRIVILEGES;"
 
-        # FIXME: If user 'misp' exists, and has a different password, the below WILL fail.
-        # Add your credentials if needed, if sudo has NOPASS, comment out the relevant lines
-        if [[ "${PACKER}" == "1" ]]; then
-        pw="Password1234"
-        else
-        pw=${MISP_PASSWORD}
-        fi
+        # Intilize misp db
+        sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "CREATE DATABASE ${DBNAME};"
+        sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "CREATE USER '${DBUSER_MISP}'@'localhost' IDENTIFIED BY '${DBPASSWORD_MISP}';"
+        sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "GRANT USAGE ON *.* to ${DBUSER_MISP}@localhost;"
+        sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "GRANT ALL PRIVILEGES on ${DBNAME}.* to '${DBUSER_MISP}'@'localhost';"
+        sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "FLUSH PRIVILEGES;"
+        
+        # Import the empty MISP database from MYSQL.sql
+        sudo cat /var/www/MISP/INSTALL/MYSQL.sql | mysql -u ${DBUSER_MISP} -p${DBPASSWORD_MISP} ${DBNAME}
 
-        expect -f - <<-EOF
-        set timeout 10
-
-        spawn sudo -k mysql_secure_installation
-        expect "*?assword*"
-        send -- "${pw}\r"
-        expect "Enter current password for root (enter for none):"
-        send -- "\r"
-        expect "Set root password?"
-        send -- "y\r"
-        expect "New password:"
-        send -- "${DBPASSWORD_ADMIN}\r"
-        expect "Re-enter new password:"
-        send -- "${DBPASSWORD_ADMIN}\r"
-        expect "Remove anonymous users?"
-        send -- "y\r"
-        expect "Disallow root login remotely?"
-        send -- "y\r"
-        expect "Remove test database and access to it?"
-        send -- "y\r"
-        expect "Reload privilege tables now?"
-        send -- "y\r"
-        expect eof
-    EOF
-        sudo apt-get purge -y expect ; sudo apt autoremove -qy
-    fi 
-
-  sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "CREATE DATABASE ${DBNAME};"
-  sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "CREATE USER '${DBUSER_MISP}'@'localhost' IDENTIFIED BY '${DBPASSWORD_MISP}';"
-  sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "GRANT USAGE ON *.* to ${DBUSER_MISP}@localhost;"
-  sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "GRANT ALL PRIVILEGES on ${DBNAME}.* to '${DBUSER_MISP}'@'localhost';"
-  sudo mysql -u ${DBUSER_ADMIN} -p${DBPASSWORD_ADMIN} -e "FLUSH PRIVILEGES;"
-  # Import the empty MISP database from MYSQL.sql
-  ${SUDO_WWW} cat ${PATH_TO_MISP}/INSTALL/MYSQL.sql | mysql -u ${DBUSER_MISP} -p${DBPASSWORD_MISP} ${DBNAME}
-
-
-
-
-
-echo "$STARTMSG mkdir -p $DATADIR/$MYSQL_DATABASE" && mkdir -p $DATADIR/mysql
-echo "$STARTMSG chown -R mysql.mysql $DATADIR" && chown -R mysql.mysql $DATADIR
-# "Other options are passed to mysqld." (so we pass all "mysqld" arguments directly here)
-gosu mysql mysql_install_db --datadir="$DATADIR" --rpm "${@:2}"
-echo "$STARTMSG Database initialized"
-
-echo "$STARTMSG Start mysqld to setup"
-start_mysql &
-# test if mysqld is ready
-check_mysql
-echo "$STARTMSG Create database $MYSQL_DATABASE, change root password and add $MYSQL_USER"
-########################################################
-$MYSQL_INIT_CMD << EOF
--- What's done in this file shouldn't be replicated
---  or products like mysql-fabric won't work
-SET @@SESSION.SQL_LOG_BIN=0;
-
--- Delete all Users except root
-DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost', '$HOSTNAME') ;
-
--- Set Password
-UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root' ;
---SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
-
--- Create Root User with %
-CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
-
--- Grant Permissions
-GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
-
--- Create MISP DB
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} ;
-
--- Create MISP DB User    
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;
-GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' ;
-
-DROP DATABASE IF EXISTS test ;
-FLUSH PRIVILEGES ;
+        ########################################################
+        # create debian.cnf
+        debian_conf=/etc/mysql/debian.cnf
+        echo "$STARTMSG Write $debian_conf"
+        # add debian.cnf File
+        cat << EOF > $debian_conf
+        #	MYSQL Configuration from DCSO
+        [client]
+        host     = localhost
+        user     = root
+        password = $MYSQL_ROOT_PASSWORD
+        socket   = /var/run/mysqld/mysqld.sock
+        [mysql_upgrade]
+        #host     = localhost
+        user     = root
+        password = $MYSQL_ROOT_PASSWORD
+        socket   = /var/run/mysqld/mysqld.sock
+        basedir  = /usr
 EOF
-
-########################################################
-# create debian.cnf
-debian_conf=/etc/mysql/debian.cnf
-echo "$STARTMSG Write $debian_conf"
-# add debian.cnf File
-cat << EOF > $debian_conf
-#	MYSQL Configuration from DCSO
-[client]
-host     = localhost
-user     = root
-password = $MYSQL_ROOT_PASSWORD
-socket   = /var/run/mysqld/mysqld.sock
-[mysql_upgrade]
-#host     = localhost
-user     = root
-password = $MYSQL_ROOT_PASSWORD
-socket   = /var/run/mysqld/mysqld.sock
-basedir  = /usr
-
-EOF
-    ########################################################
-
+        ########################################################
+    fi
 }
-
-
-
 
 ########################################################
 # START MAIN
 ########################################################
 
-
-# create an pid file for the entrypoint script.
-# entrypoint_apache start only if file is not in place.
-    echo "$STARTMSG Create pid file: ${DATADIR}/entrypoint_mariadb.pid" && touch "${DATADIR}/entrypoint_mariadb.pid" 
-# create socket folder if not exists
-    if [ ! -d "/var/run/mysqld" ];then
-        mkdir -p /var/run/mysqld
-        chown -R mysql.mysql /var/run/mysqld
-    fi
-########################################################
-# Initialize mysql daemon
-    [ -d "$DATADIR/mysql" ] && echo "$STARTMSG MariaDB is initialized"
-    [ ! -d "$DATADIR/mysql" ] && init_mysql "$@"
-########################################################
-# check volumes and upgrade if it is required
-    #echo "$STARTMSG upgrade if it is required..." && upgrade
-########################################################
-# Stop existing mysql deamon
-    echo "$STARTMSG stopping mysql..." && service mysql stop
-########################################################
-# Own the directory
-    echo "$STARTMSG chown -R mysql.mysql $DATADIR" && chown -R mysql.mysql $DATADIR
-########################################################
-# CHMOD the configuration files
-    echo "$STARTMSG chmod -R 644 /etc/mysql/*" && chmod -R 644 /etc/mysql/*
-########################################################
-# start mysql deamon
-    # delete PID file
-    echo "$STARTMSG Remove pid file: ${DATADIR}${0}.pid" && rm -v "${DATADIR}${0}.pid"
-    # start daemon
-    echo "$STARTMSG start longtime mysql..." && start_mysql "$@"
+# Start mysql deamon
+echo "$STARTMSG starting mysql..." && service mysql start
+check_mysql
+# Initialize MISP Database if not allready done
+init_mysql
+echo "$STARTMSG stopping mysql..." && service mysql stop
+echo "$STARTMSG start longtime mysql..." && start_mysql "$@"
